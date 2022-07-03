b3fbf49f0867e324c4355c042b5a7518d7162b01
VanshSingh95075 <104381790+VanshSingh95075@users.noreply.github.com>
2022-07-03 21:44:05 +0530

    Create assignment6sol.py

---
+++ assignment6sol.py
@@ -0,0 +1,55 @@
+Enter a number: 6
+6  is a perfect number
+
+#Output of question 2
+Enter a String: mam
+mam is a palindrome
+
+#Output of question 3
+Enter a number: 5
+      1 
+     1 1
+    1 2 1
+   1 3 3 1
+  1 4 6 4 1
+
+#Output of question 4
+Enter a String: the quick brown fox jumps over a lazy hazy dazy dog
+Yes 'the quick brown fox jumps over a lazy hazy dazy dog' is a panagram
+
+#Output of question 5
+Enter a hyphen separated sentence: red-yellow-blue-green
+blue-green-red-yellow
+#Output of question 6
+Student ID= 21102107
+Student ID= 21102107
+Student Name= Vansh Singh
+Student ID= 21102107
+Student Name= Vansh Singh
+Student Class= 1st Year
+
+#Output of question 7
+True
+False
+True
+False
+
+Check whether the said classes are subclasses of the built-in object class or not.
+True
+True
+
+#Output of question 8
+Enter number of elements : 6
+-4
+3
+1
+5
+6
+-11
+-4 3 1
+5 6 -11
+None
+
+#Output of question 9
+Enter the sequence of parantheses : {}[]
+{}[] - is balanced
